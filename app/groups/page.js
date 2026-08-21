9ded301194e2e8b0bfee6c4c46ba2f5e60178ac1
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-08-21 01:38:47 -0300

    feat: add groups operations page

---
+++ app/groups/page.js
@@ -0,0 +1,9 @@
+import GroupOperationsClient from "./GroupOperationsClient";
+
+export const metadata = {
+  title: "Grupos de Ofertas · Afiliado Ofertas",
+};
+
+export default function GroupsPage() {
+  return <GroupOperationsClient />;
+}
