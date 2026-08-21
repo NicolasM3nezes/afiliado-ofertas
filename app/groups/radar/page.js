49283a68d8092116812e8d73790302ee3934d405
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-08-21 01:41:47 -0300

    feat: add group-aware radar page

---
+++ app/groups/radar/page.js
@@ -0,0 +1,9 @@
+import GroupRadarClient from "./GroupRadarClient";
+
+export const metadata = {
+  title: "Radar do Grupo · Afiliado Ofertas",
+};
+
+export default function GroupRadarPage() {
+  return <GroupRadarClient />;
+}
